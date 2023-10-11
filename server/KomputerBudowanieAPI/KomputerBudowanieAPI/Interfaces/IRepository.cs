bad6382c6d453d2731c963f56329c31e4e7e466c
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-10-11 11:34:40 +0200

    Added generic repository
    I want generic repository to have basic async crud

---
+++ server/KomputerBudowanieAPI/KomputerBudowanieAPI/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace KomputerBudowanieAPI.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(int id);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task SaveChangesAsync();
+    }
+}
---
+++ server/KomputerBudowanieAPI/KomputerBudowanieAPI/Repository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using KomputerBudowanieAPI.Database;
+using KomputerBudowanieAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
+namespace KomputerBudowanieAPI.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private KomBuildDbContext _context;
+        public Repository(KomBuildDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(int id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
