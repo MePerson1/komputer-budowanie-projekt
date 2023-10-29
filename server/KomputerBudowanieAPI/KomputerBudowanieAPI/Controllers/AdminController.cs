a685310d183a3145734e66d4269311e0c5249df1
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-10-29 17:41:55 +0100

    delete all but cases not working idk

---
+++ server/KomputerBudowanieAPI/KomputerBudowanieAPI/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using KomputerBudowanieAPI.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KomputerBudowanieAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : Controller
+    {
+        readonly KomBuildDbContext _context;
+        public AdminController(KomBuildDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public IActionResult DeleteAll()
+        {
+            string[] tables = { "Cases", "CpuCoolings", "Fans", "Cpus", "GraphicCards", "Memories", "Motherboards", "Rams", "PowerSupplys" };
+            foreach (var table in tables)
+            {
+                var sql = "TRUNCATE TABLE " + table;
+                _context.Database.ExecuteSqlRaw(sql);
+            }
+
+            return Ok();
+        }
+    }
+}
