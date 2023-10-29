efe415da314445551a0a8881528637544592ee04
Skipper <janswierkowski@interia.pl>
2023-10-29 18:56:37 +0100

    Added DeleteAll in AdminController to all Tabels in databases associated with components

--- server/KomputerBudowanieAPI/KomputerBudowanieAPI/Controllers/AdminController.cs
+++ server/KomputerBudowanieAPI/KomputerBudowanieAPI/Controllers/AdminController.cs
@@ -9,21 +9,86 @@ namespace KomputerBudowanieAPI.Controllers
     public class AdminController : Controller
     {
         readonly KomBuildDbContext _context;
+        private readonly string[] tables = { "Cases", "CpuCoolings", "Fans", "Cpus", "GraphicCards", "Memories", "Motherboards", "Rams", "PowerSupplys" };
+
         public AdminController(KomBuildDbContext context)
         {
             this._context = context;
         }
-        [HttpGet]
+
+        [HttpDelete]
         public IActionResult DeleteAll()
         {
-            string[] tables = { "Cases", "CpuCoolings", "Fans", "Cpus", "GraphicCards", "Memories", "Motherboards", "Rams", "PowerSupplys" };
             foreach (var table in tables)
             {
                 var sql = "TRUNCATE TABLE " + table;
                 _context.Database.ExecuteSqlRaw(sql);
             }
-
             return Ok();
         }
+
+        [HttpDelete]
+        public IActionResult DeleteAllCases()
+        {
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE " + tables[0]);
+            return Ok();
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteAllCpuCoolings()
+        {
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE " + tables[1]);
+            return Ok();
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteAllFans()
+        {
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE " + tables[2]);
+            return Ok();
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteAllCpus()
+        {
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE " + tables[3]);
+            return Ok();
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteAllGraphicCards()
+        {
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE " + tables[4]);
+            return Ok();
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteAllMemories()
+        {
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE " + tables[5]);
+            return Ok();
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteAllMotherboards()
+        {
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE " + tables[6]);
+            return Ok();
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteAllRams()
+        {
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE " + tables[8]);
+            return Ok();
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteAllPowerSupplys()
+        {
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE " + tables[9]);
+            return Ok();
+        }
+        
     }
 }
