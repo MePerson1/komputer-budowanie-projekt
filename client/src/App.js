e28af9fb87ca5c572ac768aaf1529820388d9f5f
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-11-08 23:01:35 +0100

    NotFound added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import NavBar from "./components/shared/NavBar";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
       </header>
-      <main>
-        <div id=""></div>
+      <main className="pt-5">
+        <Routes>
+          <Route path="/" exect element={<Home />} />
+          <Route path="*" exect element={<NotFound />} />
+        </Routes>
       </main>
     </div>
   );
