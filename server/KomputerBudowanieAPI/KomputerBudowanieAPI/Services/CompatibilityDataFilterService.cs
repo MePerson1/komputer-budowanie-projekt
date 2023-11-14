04bd04f7764fd98ad50871010452f8c4033dd5d1
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-11-14 18:16:45 +0100

    file added

---
+++ server/KomputerBudowanieAPI/KomputerBudowanieAPI/Services/CompatibilityDataFilterService.cs
@@ -0,0 +1,15 @@
+﻿namespace KomputerBudowanieAPI.Services
+{
+    public class CompatibilityDataFilterService
+    {
+
+        //public async Task<ICollection<Cpu>?> FilterCpu(PcConfiguration configuration, ICollection<Cpu> cpus)
+        //{
+
+        //    if(configuration.Motherboard is not null)
+        //    {
+        //      throw CngExportPolicies   
+        //    }
+        //}
+    }
+}
