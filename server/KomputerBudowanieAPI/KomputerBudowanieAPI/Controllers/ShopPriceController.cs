f4ecae15cb35e6a2ad9f7c54dc5a93cd053ceba8
MePerson1 <112072726+MePerson1@users.noreply.github.com>
2023-12-21 23:48:17 +0100

    shopPriceController

---
+++ server/KomputerBudowanieAPI/KomputerBudowanieAPI/Controllers/ShopPriceController.cs
@@ -0,0 +1,32 @@
+﻿using KomputerBudowanieAPI.Interfaces;
+using KomputerBudowanieAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KomputerBudowanieAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopPriceController : ControllerBase
+    {
+        private readonly IGenericRepository<ShopPrice> _shopPriceRepository;
+
+        public ShopPriceController(IGenericRepository<ShopPrice> shopPriceRepository)
+        {
+            _shopPriceRepository = shopPriceRepository;
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteShopPrice(int id)
+        {
+            var shopPrice = await _shopPriceRepository.GetByIdAsync(id);
+            if (shopPrice is null)
+                return NotFound();
+            try
+            {
+                await _shopPriceRepository.Delete(shopPrice);
+                return Ok();
+            }
+            catch (Exception ex) { return BadRequest(ex.Message); }
+        }
+    }
+}
