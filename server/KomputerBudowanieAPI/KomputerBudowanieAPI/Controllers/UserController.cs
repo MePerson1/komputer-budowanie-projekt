f7dda245a6b029f49451b2a673b22864dac7756f
Skippi-engineer <83069081+Skippi-engineer@users.noreply.github.com>
2024-01-03 16:36:49 +0100

    Changed claims order

--- server/KomputerBudowanieAPI/KomputerBudowanieAPI/Controllers/UserController.cs
+++ server/KomputerBudowanieAPI/KomputerBudowanieAPI/Controllers/UserController.cs
@@ -36,9 +36,9 @@ namespace KomputerBudowanieAPI.Controllers
             {
                 var claims = new List<Claim>()
                 {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.Id),
                     new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                     new Claim(JwtRegisteredClaimNames.Email, user.Email),
-                     new Claim(JwtRegisteredClaimNames.Sub, user.Id),
                     new Claim(ClaimTypes.Name, user.UserName)
                 };
 
