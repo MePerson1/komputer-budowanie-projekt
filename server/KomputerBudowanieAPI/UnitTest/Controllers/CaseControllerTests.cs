2234c58c35b2eea0eda80c42a911ff23b32e6d31
Skippi-engineer <83069081+Skippi-engineer@users.noreply.github.com>
2024-01-10 17:12:09 +0100

    added 8 integration tests

---
+++ server/KomputerBudowanieAPI/UnitTest/Controllers/CaseControllerTests.cs
@@ -0,0 +1,129 @@
+﻿using KomputerBudowanieAPI.Dto;
+using KomputerBudowanieAPI.Models;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Controllers
+{
+    public class CaseControllerTests : IDisposable
+    {
+        private CustomWebApplicationMockFactory _factory;
+        private HttpClient _client;
+
+        public CaseControllerTests()
+        {
+            _factory = new CustomWebApplicationMockFactory();
+            _client = _factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+
+        [Fact]
+        public async Task Get_Always_ReturnAllCases()
+        {
+            // Arrange
+            var mockCases = new Case[]
+            {
+                new() { Id = 1, Name = "Obudowa Cooler Master MasterBox TD500 Mesh White (MCB-D500D-WGNN-S01)" },
+                new() { Id = 2, Name = "Obudowa Krux Heko (KRXD001)" },
+                new() { Id = 3, Name = "Obudowa Endorfy Ventum 200 ARGB (EY2A014)" },
+            }.AsQueryable();
+
+            _factory.CaseRepositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(mockCases);
+
+            // Act
+            var response = await _client.GetAsync("/api/case");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var cases = JsonConvert.DeserializeObject<IEnumerable<CaseDto>>(responseContent);
+
+            Assert.NotNull(cases);
+            Assert.Collection(cases,
+                pcCase => Assert.Equal("Obudowa Cooler Master MasterBox TD500 Mesh White (MCB-D500D-WGNN-S01)", pcCase.Name),
+                pcCase => Assert.Equal("Obudowa Krux Heko (KRXD001)", pcCase.Name),
+                pcCase => Assert.Equal("Obudowa Endorfy Ventum 200 ARGB (EY2A014)", pcCase.Name)
+            );
+        }
+
+        [Fact]
+        public async Task GetById_IfExists_ReturnsCases()
+        {
+            // Arrange
+            var mockCases = new Case[]
+            {
+                new() { Id = 1, Name = "Obudowa Cooler Master MasterBox TD500 Mesh White (MCB-D500D-WGNN-S01)" },
+                new() { Id = 2, Name = "Obudowa Krux Heko (KRXD001)" },
+                new() { Id = 3, Name = "Obudowa Endorfy Ventum 200 ARGB (EY2A014)" },
+            }.AsQueryable();
+
+            _factory.CaseRepositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync((int id) => mockCases.FirstOrDefault(pcCase => pcCase.Id == id));
+
+            // Act
+            var response = await _client.GetAsync("/api/case/2");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var pcCase = JsonConvert.DeserializeObject<CaseDto>(responseContent);
+
+            Assert.NotNull(pcCase);
+            Assert.Equal(2, pcCase.Id);
+            Assert.Equal("Obudowa Krux Heko (KRXD001)", pcCase.Name);
+        }
+
+        [Fact]
+        public async Task GetById_IfMissing_Returns404()
+        {
+            // Arrange
+            var mockCases = new Case[]
+            {
+                new() { Id = 1, Name = "Obudowa Cooler Master MasterBox TD500 Mesh White (MCB-D500D-WGNN-S01)" },
+                new() { Id = 3, Name = "Obudowa Endorfy Ventum 200 ARGB (EY2A014)" },
+            }.AsQueryable();
+
+            _factory.CaseRepositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync((int id) => mockCases.FirstOrDefault(pcCase => pcCase.Id == id));
+
+            // Act
+            var response = await _client.GetAsync("/api/case/2");
+
+            // Assert
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_WithoutToken_Returns401()
+        {
+            // Arrange
+            CaseDto caseDto = new()
+            {
+                Name = "Obudowa Endorfy Ventum 200 ARGB (EY2A014)",
+            };
+
+            _factory.CaseRepositoryMock.Setup(r => r.Create(It.Is<Case>(c => c.Name == caseDto.Name))).Verifiable();
+            _factory.CaseRepositoryMock.Setup(r => r.SaveChanges()).Verifiable();
+
+            // Act
+            var response = await _client.PostAsync("/api/case", JsonContent.Create(caseDto));
+
+            // Assert
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+    }
+}
---
+++ server/KomputerBudowanieAPI/UnitTest/Controllers/CpuCoolingControllerTests.cs
@@ -0,0 +1,129 @@
+﻿using KomputerBudowanieAPI.Dto;
+using KomputerBudowanieAPI.Models;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Controllers
+{
+    public class CpuCoolingControllerTests : IDisposable
+    {
+        private CustomWebApplicationMockFactory _factory;
+        private HttpClient _client;
+
+        public CpuCoolingControllerTests()
+        {
+            _factory = new CustomWebApplicationMockFactory();
+            _client = _factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+
+        [Fact]
+        public async Task Get_Always_ReturnAllCpuCooling()
+        {
+            // Arrange
+            var mockCpuCooling = new CpuCooling[]
+            {
+                new() { Id = 1, Name = "Chłodzenie CPU Endorfy Fera 5 Dual Fan (EY3A006)" },
+                new() { Id = 2, Name = "Chłodzenie CPU be quiet! Dark Rock 4 (BK021)" },
+                new() { Id = 3, Name = "Chłodzenie CPU Deepcool AK620 (R-AK620-BKNNMT-G)" },
+            }.AsQueryable();
+
+            _factory.CpuCoolingRepositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(mockCpuCooling);
+
+            // Act
+            var response = await _client.GetAsync("/api/cpu-cooling");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var cpuCoolings = JsonConvert.DeserializeObject<IEnumerable<CpuCoolingDto>>(responseContent);
+
+            Assert.NotNull(cpuCoolings);
+            Assert.Collection(cpuCoolings,
+                cpuCooling => Assert.Equal("Chłodzenie CPU Endorfy Fera 5 Dual Fan (EY3A006)", cpuCooling.Name),
+                cpuCooling => Assert.Equal("Chłodzenie CPU be quiet! Dark Rock 4 (BK021)", cpuCooling.Name),
+                cpuCooling => Assert.Equal("Chłodzenie CPU Deepcool AK620 (R-AK620-BKNNMT-G)", cpuCooling.Name)
+            );
+        }
+
+        [Fact]
+        public async Task GetById_IfExists_ReturnsCases()
+        {
+            // Arrange
+            var mockCpuCooling = new CpuCooling[]
+            {
+                new() { Id = 1, Name = "Chłodzenie CPU Endorfy Fera 5 Dual Fan (EY3A006)" },
+                new() { Id = 2, Name = "Chłodzenie CPU be quiet! Dark Rock 4 (BK021)" },
+                new() { Id = 3, Name = "Chłodzenie CPU Deepcool AK620 (R-AK620-BKNNMT-G)" },
+            }.AsQueryable();
+
+            _factory.CpuCoolingRepositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync((int id) => mockCpuCooling.FirstOrDefault(cooling => cooling.Id == id));
+
+            // Act
+            var response = await _client.GetAsync("/api/cpu-cooling/2");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var cpuCooling = JsonConvert.DeserializeObject<CpuCoolingDto>(responseContent);
+
+            Assert.NotNull(cpuCooling);
+            Assert.Equal(2, cpuCooling.Id);
+            Assert.Equal("Chłodzenie CPU be quiet! Dark Rock 4 (BK021)", cpuCooling.Name);
+        }
+
+        [Fact]
+        public async Task GetById_IfMissing_Returns404()
+        {
+            // Arrange
+            var mockCpuCooling = new CpuCooling[]
+            {
+                new() { Id = 1, Name = "Chłodzenie CPU Endorfy Fera 5 Dual Fan (EY3A006)" },
+                new() { Id = 3, Name = "Chłodzenie CPU Deepcool AK620 (R-AK620-BKNNMT-G)" },
+            }.AsQueryable();
+
+            _factory.CpuCoolingRepositoryMock.Setup(r => r.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync((int id) => mockCpuCooling.FirstOrDefault(cooling => cooling.Id == id));
+
+            // Act
+            var response = await _client.GetAsync("/api/cpu-cooling/2");
+
+            // Assert
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_WithoutToken_Returns401()
+        {
+            // Arrange
+            CpuCoolingDto cpuCoolingDto = new()
+            {
+                Name = "Chłodzenie CPU Endorfy Fera 5 Dual Fan (EY3A006)",
+            };
+
+            _factory.CpuCoolingRepositoryMock.Setup(r => r.Create(It.Is<CpuCooling>(c => c.Name == cpuCoolingDto.Name))).Verifiable();
+            _factory.CpuCoolingRepositoryMock.Setup(r => r.SaveChanges()).Verifiable();
+
+            // Act
+            var response = await _client.PostAsync("/api/cpu-cooling", JsonContent.Create(cpuCoolingDto));
+
+            // Assert
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+    }
+}
