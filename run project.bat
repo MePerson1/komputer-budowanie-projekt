1be764ebfd77ae2c0eaac575f4bc00705c9ae631
Szajkowski <84507754+Szajkowski@users.noreply.github.com>
2024-01-14 18:45:20 +0100

    Update run project.bat

--- run project.bat
+++ run project.bat
@@ -3,11 +3,11 @@
 rem Step 1: Navigate to the server directory
 cd server\KomputerBudowanieAPI\KomputerBudowanieAPI
 
-rem Step 2: Run "dotnet run" in the first console
-start cmd /k dotnet run
+rem Step 2: Start server in the first console
+start cmd /c dotnet run
 
 rem Step 3: Navigate back to the client directory
 cd ..\..\..\client
 
-rem Step 4: Run "npm run start" in the second console
-start cmd /k npm run start
+rem Step 4: Install npm dependencies and then start client in the second console
+start cmd /c "npm install && npm run start"
